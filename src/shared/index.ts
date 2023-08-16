24a25df0bcea51194c1193dee7d1f034ba5db1c3
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-08-16 08:22:28 +0200

    Zustand and Global state management

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
 export { Navbar } from "./components/core/Navbar";
 export { ServerError } from "./components/core/ServerError";
 export { Spinner } from "./components/core/Spinner";
+export { CartPanel } from "./components/core/CartPanel";
